8fd32a71d05b3c60e09bc0fc7df0ddc11f8d2637
Yudai Hayashi <yudai0913haya@gmail.com>
2020-09-21 21:19:09 +0900

    Formatted account app with black

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,20 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.views import (
-    LoginView, LogoutView
-)
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .forms import LoginForm, SignUpForm
 
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
 class Login(LoginView):
     form_class = LoginForm
-    template_name = 'registration/login.html'
+    template_name = "registration/login.html"
 
 
 class Logout(LogoutView):
-    template_name = 'registration/logout.html'
+    template_name = "registration/logout.html"
